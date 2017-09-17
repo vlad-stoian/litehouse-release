f5457ebeba25c14c8d1396d555f8decd003bd607
Vlad Stoian <vstoian@pivotal.io>
2017-09-17 21:52:28 +0100

    Add bosh-lite-template to assets
    - Starting to interpolate a little bit

--- src/github.com/vlad-stoian/litehouse/adapter/manifest_generator.go
+++ src/github.com/vlad-stoian/litehouse/adapter/manifest_generator.go
@@ -1,8 +1,14 @@
 package adapter
 
 import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+
+	boshtpl "github.com/cloudfoundry/bosh-cli/director/template"
 	"github.com/pivotal-cf/on-demand-services-sdk/bosh"
 	"github.com/pivotal-cf/on-demand-services-sdk/serviceadapter"
+	yaml "gopkg.in/yaml.v2"
 )
 
 type ManifestGenerator struct{}
@@ -15,13 +21,27 @@ func (mg ManifestGenerator) GenerateManifest(
 	previousPlan *serviceadapter.Plan,
 ) (bosh.BoshManifest, error) {
 
-	return bosh.BoshManifest{
-		Name:     "litehouse",
-		Releases: []bosh.Release{},
-		Stemcells: []bosh.Stemcell{{
-			Alias:   "stemcell-alias",
-			OS:      "stemcell-os",
-			Version: "stemcell-version",
-		}},
-	}, nil
+	boshLiteTemplatePath := filepath.FromSlash("../assets/bosh-lite-template.yml")
+	contents, err := ioutil.ReadFile(boshLiteTemplatePath)
+	if err != nil {
+		return bosh.BoshManifest{}, fmt.Errorf("Error reading from: %v", boshLiteTemplatePath)
+	}
+
+	template := boshtpl.NewTemplate(contents)
+	variables := boshtpl.StaticVariables{
+		"external_ip": "1234",
+	}
+
+	// something := cfgtypes.NewValueGeneratorConcrete(cfgtypes.NewVarsCertLoader(vars))
+	// https://github.com/cloudfoundry/bosh-cli/blob/06eede24ca808e599a73fd38251ed84d0c506e07/cmd/var_flags.go
+
+	boshLiteManifestBytes, _ := template.Evaluate(variables, nil, boshtpl.EvaluateOpts{})
+
+	var boshLiteManifest bosh.BoshManifest
+	err = yaml.Unmarshal(boshLiteManifestBytes, &boshLiteManifest)
+	if err != nil {
+		return bosh.BoshManifest{}, fmt.Errorf("Error unmarshalling: %v", contents)
+	}
+
+	return boshLiteManifest, nil
 }
--- src/github.com/vlad-stoian/litehouse/integration_tests/adapter_test.go
+++ src/github.com/vlad-stoian/litehouse/integration_tests/adapter_test.go
@@ -4,6 +4,8 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
+	"github.com/pivotal-cf/on-demand-services-sdk/bosh"
+	yaml "gopkg.in/yaml.v2"
 )
 
 var _ = Describe("adapter commands", func() {
@@ -16,8 +18,8 @@ var _ = Describe("adapter commands", func() {
 			arbitraryParams   string
 			previousManifest  string
 			serviceDeployment string
-			// generatedManifest *bosh.BoshManifest
-			// unmarshallErr     error
+			generatedManifest *bosh.BoshManifest
+			unmarshallErr     error
 		)
 
 		BeforeEach(func() {
@@ -52,10 +54,38 @@ var _ = Describe("adapter commands", func() {
 
 		JustBeforeEach(func() {
 			session = execBin("generate-manifest", serviceDeployment, plan, arbitraryParams, previousManifest, previousPlan)
+			generatedManifest = new(bosh.BoshManifest)
+			unmarshallErr = yaml.Unmarshal(session.Out.Contents(), generatedManifest)
 		})
 
 		It("completes successfully", func() {
 			Expect(session.ExitCode()).To(Equal(0))
+		})
+
+		It("produces valid bosh manifest", func() {
+			Expect(unmarshallErr).ToNot(HaveOccurred())
+		})
+
+		It("contains bosh instance group", func() {
+			Expect(generatedManifest.InstanceGroups).To(HaveLen(1))
+			Expect(generatedManifest.InstanceGroups[0].Name).To(Equal("bosh"))
+		})
+
+		It("contains correct jobs", func() {
+			Expect(generatedManifest.InstanceGroups[0].Jobs).To(HaveLen(8))
+
+			expectedJobNames := []string{"nats", "postgres-9.4", "blobstore", "director", "health_monitor", "warden_cpi", "garden", "user_add"}
+			for i, actualJob := range generatedManifest.InstanceGroups[0].Jobs {
+				Expect(actualJob.Name).To(Equal(expectedJobNames[i]))
+			}
+		})
+
+		XIt("contains no unused variables", func() {
+			Expect(generatedManifest.Variables).To(HaveLen(0))
+		})
+
+		It("has interpolated the external_ip correctly", func() {
+			Expect(generatedManifest.InstanceGroups[0].Networks[1].StaticIPs[0]).ToNot(Equal("((external_ip))"))
 		})
 	})
 })
